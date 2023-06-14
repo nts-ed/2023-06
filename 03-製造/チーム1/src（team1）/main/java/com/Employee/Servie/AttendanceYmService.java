a6af91b0535bf303fe7aac28898927233f7ce392
陳徐輝 <134343723+Allen9803@users.noreply.github.com>
2023-06-14 14:04:16 +0900

    Add files via upload

---
+++ 03-製造/チーム1/src（team1）/main/java/com/Employee/Servie/AttendanceYmService.java
@@ -0,0 +1,13 @@
+package com.sym.service;
+
+import com.sym.entity.AttendanceYm;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author cxh98
+* @description 针对表【t_attendance_ym(勤怠年月)】的数据库操作Service
+* @createDate 2023-06-14 09:51:24
+*/
+public interface AttendanceYmService extends IService<AttendanceYm> {
+
+}
