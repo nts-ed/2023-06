82c1aa5d21610da85e0b4f0b9568b306282f38b4
陳徐輝 <134343723+Allen9803@users.noreply.github.com>
2023-06-14 15:45:49 +0900

    Update AttendanceALController.java

--- 03-製造/チーム1/src（team1）/main/java/com/Employee/Controller/AttendanceALController.java
+++ 03-製造/チーム1/src（team1）/main/java/com/Employee/Controller/AttendanceALController.java
@@ -1,9 +1,26 @@
-package com.sym.controller;
+package com.Employee.controller;
 
+import com.sym.entity.AttendanceAL;
+import com.sym.service.AttendanceALService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-//请求处理类
+import java.util.List;
+
 @RestController
 public class AttendanceALController {
+
+	@Autowired
+	private AttendanceALService attendanceALService;
+
+	@PostMapping("/attendanceAL")
+	public List<AttendanceAL> attendanceALList(@RequestBody Integer employeeId, Integer deptId,
+	                                           String employeeName, Integer applyId, String start,
+	                                           String end) {
+		return attendanceALService.attendanceALList(employeeId, deptId, employeeName, applyId, start, end);
+
+	}
 
 }
