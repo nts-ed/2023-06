494746d030f92bb5371ab0ccb4db3e5455b5fbc6
GaoJingyunan <134575345+GaoJingyunan@users.noreply.github.com>
2023-06-14 16:32:38 +0900

    Add files via upload

--- 03-製造/チーム1/src（team1）/main/java/com/Employee/Controller/AttendanceController.java
+++ 03-製造/チーム1/src（team1）/main/java/com/Employee/Controller/AttendanceController.java
@@ -1,8 +1,6 @@
 package com.example.demo.controller;
-
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,28 +9,24 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.dto.AttendanceDTO;
 import com.example.demo.service.AttendanceService;
 
-@RestController
-@RequestMapping("/api/attendance")
-public class AttendanceController {
+import java.util.List;
+
+	@RestController
+	@RequestMapping("/api/attendance")
+	public class AttendanceController {
 
 	    @Autowired
 	    private AttendanceService attendanceService;
 
 	    @GetMapping
-	    public List<AttendanceDTO> getAttendanceData(@RequestParam("date") String date) {
-	        // 在此处调用AttendanceService中的方法，从数据库中查询2023年6月的出勤数据
-	    	 List<AttendanceDTO> attendanceData = attendanceService.getAttendanceDataForDate(date);
-	        for (AttendanceDTO attendance : attendanceData) {
-	            if (attendance.getHOLIDAY_FLG() == 1) {
-	                attendance.setRowClass("attendance-row holiday");
-	            } else {
-	                attendance.setRowClass("attendance-row");
-	            }
-	        }
+	    public AttendanceDTO[] getAttendanceDataForDate(@RequestParam("date") String date) {
+	        List<AttendanceDTO> attendanceData = attendanceService.getAttendanceDataForDate(date);
+	        AttendanceDTO[] attendanceArray = new AttendanceDTO[attendanceData.size()];
+	        attendanceData.toArray(attendanceArray);
 	        for (AttendanceDTO attendance : attendanceData) {
 	            System.out.println(attendance.toString());
 	        }
-
-	        return attendanceData;
+	        return attendanceArray;
 	    }
+	
 	}
