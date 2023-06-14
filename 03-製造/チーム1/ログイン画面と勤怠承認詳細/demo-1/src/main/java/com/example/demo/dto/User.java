54a16a7d3889d193a64aaabaf280ae82cb5ff158
GaoJingyunan <134575345+GaoJingyunan@users.noreply.github.com>
2023-06-14 09:19:10 +0900

    できた部分をアップします。

---
+++ 03-製造/チーム1/ログイン画面と勤怠承認詳細/demo-1/src/main/java/com/example/demo/dto/User.java
@@ -0,0 +1,17 @@
+package com.example.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(includeFieldNames = true)
+public class User {
+private Integer id; 
+private String password;
+
+}
