c4b982f8ba5624b0d24ade49f9195b696c440df0
nagashimakaito <134575738+nagashimakaito@users.noreply.github.com>
2023-06-14 13:55:01 +0900

    途中ですが提出します
    途中ですが提出します

---
+++ 03-製造/チーム1/src（team1）/main/java/com/Employee/Controller/EmployeeController.java
@@ -0,0 +1,30 @@
+package com.nts._ed.ks.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nts._ed.ks.repository.EmployeeRepository;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Employee getEmployeeById(@PathVariable Long id) {
+        return employeeRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid employee ID: " + id));
+    }
+}
