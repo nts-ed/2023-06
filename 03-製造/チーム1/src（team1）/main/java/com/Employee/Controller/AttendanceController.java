d52e25b75edb2824b0fe5da90f716cf925e09540
GaoJingyunan <134575345+GaoJingyunan@users.noreply.github.com>
2023-06-15 10:16:27 +0900

    更新しました。

--- 03-製造/チーム1/src（team1）/main/java/com/Employee/Controller/AttendanceController.java
+++ 03-製造/チーム1/src（team1）/main/java/com/Employee/Controller/AttendanceController.java
@@ -1,17 +1,18 @@
 package com.example.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+
 
 import com.example.demo.dto.AttendanceDTO;
 import com.example.demo.service.AttendanceService;
 
 import java.util.List;
 
-	@RestController
+    @Controller
 	@RequestMapping("/api/attendance")
 	public class AttendanceController {
 
@@ -19,14 +20,14 @@ import java.util.List;
 	    private AttendanceService attendanceService;
 
 	    @GetMapping
-	    public AttendanceDTO[] getAttendanceDataForDate(@RequestParam("date") String date) {
+	    public String getAttendanceDataForDate(@RequestParam("date") String date, Model model) {
 	        List<AttendanceDTO> attendanceData = attendanceService.getAttendanceDataForDate(date);
-	        AttendanceDTO[] attendanceArray = new AttendanceDTO[attendanceData.size()];
-	        attendanceData.toArray(attendanceArray);
 	        for (AttendanceDTO attendance : attendanceData) {
 	            System.out.println(attendance.toString());
 	        }
-	        return attendanceArray;
-	    }
+	    model.addAttribute("attendanceData",attendanceData);
+       return "aa/text";	   
+       }
 	
-	}
+}
+	    
