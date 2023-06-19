608b1138d78795bed1b6492619f4e1b8e2a56992
陳徐輝 <134343723+Allen9803@users.noreply.github.com>
2023-06-19 15:42:36 +0900

    Add files via upload

--- 03-製造/チーム1/src（team1）/main/java/com/Employee/repo/AttendanceALMapper.java
+++ 03-製造/チーム1/src（team1）/main/java/com/Employee/repo/AttendanceALMapper.java
@@ -1,18 +1,15 @@
-package com.Employee.mapper;
+package com.sym.mapper;
 
 
-
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.sym.entity.AttendanceAL;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 @Mapper
-public interface AttendanceALMapper {
-	public List<AttendanceAL> attendanceALList(Integer employeeId, Integer deptId, String employeeName, Integer applyId, String start, String end);
+public interface AttendanceALMapper extends BaseMapper<AttendanceAL> {
+
+	List<AttendanceAL> attendanceALList(Integer employeesId, Integer deptId, String employeesName, Integer applyId, String start, String end);
 }
-
-
-
-
-
