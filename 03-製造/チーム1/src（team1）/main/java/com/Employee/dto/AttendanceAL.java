ba192cc6babe66183c0c365c807c3101692a3fc8
陳徐輝 <134343723+Allen9803@users.noreply.github.com>
2023-06-19 15:41:54 +0900

    Add files via upload

--- 03-製造/チーム1/src（team1）/main/java/com/Employee/dto/AttendanceAL.java
+++ 03-製造/チーム1/src（team1）/main/java/com/Employee/dto/AttendanceAL.java
@@ -1,19 +1,29 @@
-package com.Employee.dto;
+package com.sym.entity;
 
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serial;
+import java.io.Serializable;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-public class AttendanceAL {
-	private Integer employeeId;
+public class AttendanceAL implements Serializable {
+	@Serial
+	private static final long serialVersionUID = 1L;
+
+	@TableId(type = IdType.AUTO)
+	private Integer employeesId;
 	private String attendanceYm;
-	private String employeeName;
+	private String employeesName;
 	private Integer deptId;
-	private Integer workHoursMonth;
+	private Integer workingHoursMonth;
 	private Integer restHoursMonth;
 	private Integer overtimeHoursMonth;
 	private Integer absenceHoursMonth;
