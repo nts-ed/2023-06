288221cbc1429e35661d5de05d904d5baee07096
GaoJingyunan <134575345+GaoJingyunan@users.noreply.github.com>
2023-06-19 17:45:15 +0900

    Add files via upload

--- 03-製造/チーム1/src（team1）/main/java/com/Employee/Servie/AttendanceService.java
+++ 03-製造/チーム1/src（team1）/main/java/com/Employee/Servie/AttendanceService.java
@@ -2,6 +2,7 @@ package com.example.demo.service;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +14,18 @@ public class AttendanceService {
     @Autowired
     private AttendanceMapper attendanceMapper;
 
-    public List<AttendanceDTO> getAttendanceDataForDate(String date) {
-        return attendanceMapper.getAttendanceDataForDate(date);
+    public List<AttendanceDTO> getAttendanceDataForIdAndDate(Integer id, String date) {
+        return attendanceMapper.getAttendanceDataForIdAndDate(id, date);
+    }
+
+    public void updateAttendanceStatus(Integer id, String date, String action) {
+        // 调用相应的更新方法来更新申请状态
+        attendanceMapper.updateAttendanceStatus(id, date, action);
     }
 }
+
+
+
+
+
+
