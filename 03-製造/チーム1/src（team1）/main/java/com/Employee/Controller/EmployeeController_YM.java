b27cc3f9ead3d620061e868900ba7a589f8ed935
JyouGyouhi <134584200+JyouGyouhi@users.noreply.github.com>
2023-06-19 16:57:53 +0900

    Add files via upload

---
+++ 03-製造/チーム1/src（team1）/main/java/com/Employee/Controller/EmployeeController_YM.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.Employee;
+import com.example.demo.service.EmployeeService;
+
+@RestController
+public class EmployeeController {
+    
+    @Autowired
+    public EmployeeService attendanceService;
+    
+    @GetMapping("/attendance")
+    public List<Employee> list(){
+    	return attendanceService.list();
+    }
+}
+
