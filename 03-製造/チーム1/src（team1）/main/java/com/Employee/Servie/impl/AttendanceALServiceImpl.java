3608f7f2f41bdaf64da3291c8400c642e03e4de6
陳徐輝 <134343723+Allen9803@users.noreply.github.com>
2023-06-19 15:41:28 +0900

    Add files via upload

---
+++ 03-製造/チーム1/src（team1）/main/java/com/Employee/Servie/impl/AttendanceALServiceImpl.java
@@ -0,0 +1,23 @@
+package com.sym.service.impl;
+
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.sym.entity.AttendanceAL;
+import com.sym.mapper.AttendanceALMapper;
+import com.sym.service.AttendanceALService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AttendanceALServiceImpl extends ServiceImpl<AttendanceALMapper, AttendanceAL> implements AttendanceALService {
+
+	@Autowired
+	private AttendanceALMapper attendanceALMapper;
+
+	@Override
+	public List<AttendanceAL> attendanceALList(Integer employeesId, Integer deptId, String employeesName, Integer applyId, String start, String end) {
+		return attendanceALMapper.attendanceALList(employeesId, deptId, employeesName, applyId, start, end);
+	}
+}
