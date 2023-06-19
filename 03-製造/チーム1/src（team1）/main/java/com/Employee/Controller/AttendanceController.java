c596bdd6f80e916c917f0ee15cffa7add23c9a39
nagashimakaito <134575738+nagashimakaito@users.noreply.github.com>
2023-06-19 17:29:18 +0900

    前回の修正版
    前回の修正版

--- 03-製造/チーム1/src（team1）/main/java/com/Employee/Controller/AttendanceController.java
+++ 03-製造/チーム1/src（team1）/main/java/com/Employee/Controller/AttendanceController.java
@@ -1,33 +1,62 @@
-package com.example.demo.controller;
-import org.springframework.beans.factory.annotation.Autowired;
+package com.example.controllers;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
+/*
+@Controller
+public class AttendanceController {
+ private final AttendanceRepository attendanceRepository;
 
-import com.example.demo.dto.AttendanceDTO;
-import com.example.demo.service.AttendanceService;
+ @Autowired
+ public AttendanceController(AttendanceRepository attendanceRepository) {
+     this.attendanceRepository = attendanceRepository;
+ }
 
-import java.util.List;
+ @GetMapping("/")
+ public String showAttendance(Model model) {
+     LocalDate currentDate = LocalDate.now();
+     List<AttendanceRepository> attendanceList = attendanceRepository.findByDate(currentDate);
+     model.addAttribute("attendanceList", attendanceList);
+     return "attendance";
+*/
+     
+     
+     @Controller
+     public class AttendanceController {
 
-    @Controller
-	@RequestMapping("/api/attendance")
-	public class AttendanceController {
+         @GetMapping("/attendance/monthly")
+         public String displayMonthlyAttendance() {
+             // 勤怠情報一覧（月別）画面の表示ロジック
+             return "monthly_attendance";
+         }
 
-	    @Autowired
-	    private AttendanceService attendanceService;
+         @GetMapping("/attendance/daily")
+         public String displayDailyAttendance() {
+             // 勤怠情報一覧（日別）画面の表示ロジック
+             return "daily_attendance";
+         }
 
-	    @GetMapping
-	    public String getAttendanceDataForDate(@RequestParam("date") String date, Model model) {
-	        List<AttendanceDTO> attendanceData = attendanceService.getAttendanceDataForDate(date);
-	        for (AttendanceDTO attendance : attendanceData) {
-	            System.out.println(attendance.toString());
-	        }
-	    model.addAttribute("attendanceData",attendanceData);
-       return "aa/text";	   
-       }
-	
-}
-	    
+         @GetMapping("/attendance")
+         public String redirectToMonthlyAttendance() {
+             return "redirect:/attendance/monthly";
+         }
+         
+         
+         @GetMapping("/attendance")
+         public String displayAttendance() {
+              // 勤怠情報一覧画面の表示ロジック
+              return "attendance";
+             }
+
+          @GetMapping("/attendance/new")
+          public String displayAttendanceFormForAdd() {
+               // 勤怠情報追加画面の表示ロジック
+              return "attendance_form";
+             }
+
+          @GetMapping("/attendance/edit")
+          public String displayAttendanceFormForEdit() {
+              // 勤怠情報修正画面の表示ロジック
+              return "attendance_form";
+         }
+ }
