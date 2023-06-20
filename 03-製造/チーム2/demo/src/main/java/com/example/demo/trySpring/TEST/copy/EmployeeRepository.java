310b494adc7406d10186fb6253fc0e9a86930348
arufa142 <91296309+arufa142@users.noreply.github.com>
2023-06-20 10:12:10 +0900

    Delete 03-製造/チーム2/demo/src/main/java/com/example/demo/trySpring/TEST directory

--- 03-製造/チーム2/demo/src/main/java/com/example/demo/trySpring/TEST/copy/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.entity.*;
-
-public interface EmployeeRepository extends JpaRepository <Employee, String> {
-
-    Employee findByEmployeeID(String employeeID);
-}
