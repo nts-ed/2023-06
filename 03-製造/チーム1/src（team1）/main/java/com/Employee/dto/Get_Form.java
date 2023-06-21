c9e43997b5b0b28c53b81341281b7dc16b79bb54
ChinoAnkoro <130035905+ChinoAnkoro@users.noreply.github.com>
2023-06-21 10:48:48 +0900

    Add files via upload

---
+++ 03-製造/チーム1/src（team1）/main/java/com/Employee/dto/Get_Form.java
@@ -0,0 +1,12 @@
+package Employees.form;
+
+public class Get_Form {
+	private String category;
+	private String date;
+	
+	public String getCategory() { return category; }
+	public void setCategory(String category) { this.category = category; }
+	
+	public String getDate() { return date; }
+	public void setDate(String date) { this.date = date; }	
+}
